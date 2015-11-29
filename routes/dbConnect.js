1c645d16296dcb23c3bf3b4ddaa3569c422fc768
Daniel Słaby <pufek93@gmail.com>
2015-11-29 11:03:46 +0100

    podlaczenie do bazy danych

---
+++ routes/dbConnect.js
@@ -0,0 +1,32 @@
+/**
+ * Created by Slaby on 29.11.2015.
+ */
+/**
+ *
+ * @param callback funkcja wywolana po polaczeniu z baza. Pamietaj o db.close()
+ */
+var mongojs = require('mongojs');
+var db;
+exports.connect = function (callback) {
+    if (process.env.OPENSHIFT_NODEJS_IP === undefined) {
+        console.log("Projekt podlaczony lokalnie");
+        db = mongojs('admin:CbginbLnch_c@localhost/projekt', ['test']);
+    }else{
+        console.log("Projekt podlaczony zdalnie");
+        db = mongojs('admin:CbginbLnch_c@127.9.142.2/projekt', ['test']);
+    }
+
+    if (callback) {
+        callback(db);
+    } else {
+        db.close();
+    }
+};
+
+exports.disconnect = function (callback) {
+    db.close();
+    console.log("Disconnected from database");
+    if (callback) {
+        callback();
+    }
+};
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,28 @@
 var express = require('express');
 var router = express.Router();
-
+var mongo = require('./dbConnect.js');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-}); 
-router.all('/testowo', function(req, res, next) {
-  res.json({"a":"dane"});
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Express'});
+});
+// http://localhost:8080/testowo
+router.all('/testowo', function (req, res, next) {
+    mongo.connect(function (db) {
+        "use strict";
+        db.test.find({}, function (err, data) {
+            if (err) {
+                console.error(err);
+                res.json("error");
+            }
+            res.json(data);
+        });
+
+
+    });
+
 });
 module.exports = router;
+
+
+

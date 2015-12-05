bda4aa122a89282bcf509e02d4d19670a0e4e9f0
Pawel Bozek <pawel.jan.bozek@gmail.com>
2015-12-06 00:45:01 +0100

    Klient, wgrywanie obrazków.. Problem z identyfikowaniem, czy to rzeczywiście jest obrazek.

--- public/modules/cms/controllers/imageUploadCtrl.js
+++ public/modules/cms/controllers/imageUploadCtrl.js
@@ -8,5 +8,27 @@ function imageUploadCtrl($scope,FileUploader) {
     // https://github.com/nervgh/angular-file-upload/wiki/Introduction
     $scope.uploader = new FileUploader();
 
+    $scope.uploader.filters.push({
+        name: 'debug_printInfo',
+        fn: function(item /*{File|FileLikeObject}*/, options) {
+            alert("item.type " + item.type); //MIME type
+            alert("item.size " + item.size); //File size in bytes
+            alert("item.formData " + item.formData);
+            return true;
+        }
+    });
+    $scope.uploader.filters.push({
+        name: 'imageFilter',
+        fn: function(item /*{File|FileLikeObject}*/, options) {
+            var type = '|' + item.type.slice(item.type.lastIndexOf('/') + 1) + '|';
+            return '|jpg|png|jpeg|bmp|gif|'.indexOf(type) !== -1;
+        }
+    });
+    $scope.uploader.filters.push({
+        name: 'sizeFilter',
+        fn: function(item /*{File|FileLikeObject}*/, options) {
+            return (item.size > 0 && item.size <= 3000000);
+        }
+    });
 
 }

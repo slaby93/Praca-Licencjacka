42c619297076724c9e717c1d22a52fc4bd7cbd24
Daniel Słaby <pufek93@gmail.com>
2015-12-21 16:30:10 +0100

    proba wgrania pliku konfiguracyjnego po stronie serwera ( i ignorowanie go przy pobieraniu).

--- app.js
+++ app.js
@@ -12,10 +12,17 @@ var cms = require('./routes/cms');
 
 var app = express();
 
+try {
+    var konfiguracja_env = require('environment.js');
+    console.log("Plik konfiguracyjny znaleziono");
+} catch (err) {
+    console.error("Nie znaleziono pliku konfiguracyjnego");
+}
+
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, './public')));
 
@@ -24,10 +31,10 @@ app.use('/user', users);
 app.use('/cms', cms);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handlers
@@ -35,17 +42,17 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.send('error'+ err.message );
-  });
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.send('error' + err.message);
+    });
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.send('error'+ err.message );
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.send('error' + err.message);
 });
 
 

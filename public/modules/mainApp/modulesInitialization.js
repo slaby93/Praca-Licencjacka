d58e0468f5ee910e16498082272793234e85b071
Damian Michalski <dammic6988@gmail.com>
2016-03-15 10:42:16 +0100

    Poprawka do ostatniego commita NIEZBĘDNA!

--- public/modules/mainApp/modulesInitialization.js
+++ public/modules/mainApp/modulesInitialization.js
@@ -7,7 +7,7 @@ import * as angular from 'angular';
 import * as ui_router from 'angular-ui-router';
 import * as angular_local_storage from 'angular-local-storage';
 import * as angular_material from 'angular-material';
-import * as angular-material-data-table from 'angular-material-data-table'
+import * as angular_material_data_table from 'angular-material-data-table'
 
 // -----------------------------------------------------   Config imports        ------------------------------------------------------------------------------------------
 import routing from './routing';

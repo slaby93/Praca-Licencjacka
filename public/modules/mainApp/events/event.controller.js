e9c6e41f123de88b47979f745bcbc9af44d725df
Daniel Słaby <pufek93@gmail.com>
2016-04-12 21:26:27 +0200

    Added first 3d element

--- public/modules/mainApp/events/event.controller.js
+++ public/modules/mainApp/events/event.controller.js
@@ -3,11 +3,30 @@
  */
 class EventController {
 
-    constructor($scope, $log,$stateParams) {
+    constructor($scope, $log, $stateParams) {
         let self = this;
         self.$scope = $scope;
         self.$l = $log;
-        $log.debug("Params",$stateParams);
+        $log.debug("Params", $stateParams);
+        self.defaultValues();
+    }
+
+    defaultValues() {
+        let self = this;
+        self.state = "add";
+    }
+
+    test() {
+        let self = this;
+        if (self.state === "add") {
+            self.state = "edit";
+        } else {
+            // $('.switchToEdit').css('transform', 'rotateY(90deg)');
+            // self.$scope.$evalAsync(()=> {
+                self.state = "add";
+            // },100);
+        }
+
     }
 }
 

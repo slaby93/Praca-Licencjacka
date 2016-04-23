f4e99e5d2574940b2f9f1422f05c2d1ddaf2f3f4
Damian Michalski <dammic6988@gmail.com>
2016-04-23 20:20:19 +0200

    Added new tables to the user profile: active events and archive events. Fixed some css issues with overflow and general appearance. Fixed the not-always working pagination (NaN error). Added the fundaments for comments table.

--- public/modules/directives/userCommentsTable/userCommentsTable.directive.js
+++ public/modules/directives/userCommentsTable/userCommentsTable.directive.js
@@ -15,7 +15,47 @@ function userCommentsTable() {
 class UserCommentsTableController {
     constructor() {
         let self = this;
+		
+		self.comments = [];
+		
+		self.totalResults = 0;
+		self.totalPages = 1;
+		self.limit = 4;
+		
+		self.positives = 2;
+		self.negatives = 1;
+		self.commentsCount = 4;
+		self.organized = 1;
+		self.positivePercentage = self.positives/self.commentsCount * 100 ;
+		self.getComments();
+	}
+	
+	paginate(page, limit){
+		let self = this;
+		console.log("Page: "+page+ " limit: "+limit);
+	}
+	
+	getComments(){
+        let self = this;
 
+		self.totalResults = 4;
+		self.totalPages = Math.ceil(self.totalResults / self.limit);
+		self.comments = [
+			{	
+				
+			},
+			{	
+				
+			},
+			{	
+				
+			},
+			{
+				
+			}
+		];
+		
+		
 	}
 	
 

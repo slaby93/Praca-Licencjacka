ba9f301b89643298507788bfb5314fc2d91cb618
Damian Michalski <dammic6988@gmail.com>
2016-05-01 23:49:07 +0200

    [Before-bed commit] Added foundation for events (changed Event class, changed some things in database). Somehow, the route still does not work.

--- public/modules/services/eventService.js
+++ public/modules/services/eventService.js
@@ -6,25 +6,58 @@ import Moment from 'moment';
 
 class EventService {
 
-    constructor($log) {
+    constructor($log, $http, $state, $q) {
         let self = this;
         self.$l = $log;
+        self.$http = $http;
+        self.$state = $state;
+		self.$q = $q;
+		
         self.defaultValues();
     }
 
     defaultValues() {
         let self = this;
-        self.events = [];
     };
 
-
-    get events() {
-        return this._events;
-    }
-
-    set events(value) {
-        this._events = value;
-    }
+	addEvent(passedEvent) {
+		console.log("lol");
+        let self = this;
+		
+		let passedEvent1 = {
+			"author" : "slaby",
+			"city" : "krakow",
+			"createdDate" : new Date(2016, 3, 17, 0, 0),
+			"date" : new Date(2016, 3, 19, 0, 0),
+			"defaultEventIcon" : true,
+			"defaultEventImage" : true,
+			"eventInfo" : {
+				"description" : "To wydarzenie bedzie wspaniale!",
+				"payment" : 30,
+				"ownEquipment" : false,
+				"experienced" : true,
+				"usersLimit" : 20
+			},
+			"isActive" : false,
+			"localization" : {
+				"latitude" : 50.0543231,
+				"longitude" : 50.0543231
+			},
+			"participants" : [ 
+				{
+					"name" : "test"
+				}
+			],
+			"region" : "malopolskie"
+		};
+        let promise = self.$q.defer();
+        self.$http.post("/addEvent", passedEvent1, {skipAuthorization: true}).then((received) => {
+            promise.resolve(received);
+        }, (err) => {
+            promise.reject(err);
+        });
+        return promise.promise;
+    };
 
 }
 export default EventService;
--- routes/event.js
+++ routes/event.js
@@ -27,4 +27,191 @@ router.post('/icon', auth, guard.check('user'), function (req, res, next) {
 	});
 });
 
+
+
+router.post('/addEvent', auth, guard.check('user'), function (req, res, next) {
+	console.log("lol1");
+	tokenHandler.verifyToken(req.payload, function(result) {
+		if (!result) {
+			res.status(401).send("Token is invalid");
+			return;
+		}
+		
+		var event = req.body;
+		mongo.connect("serwer", ["user"], function (db) {
+			
+			//dodanie eventu do bazy
+			db.event.insert({
+				"author" : event.author,
+				"createdDate" : event.createdDate,
+				"date" : event.date,
+				"region" : event.region,
+				"city" : event.city,
+				"isActive" : true,
+				"localization" : {
+					"latitude" : event.localization.latitude,
+					"longitude" : event.localization.longitude
+				},
+				"eventInfo" : {
+					"description" : event.description,
+					"payment" : event.payment,
+					"ownEquipment" : event.ownEquipment,
+					"experienced" : event.experienced,
+					"usersLimit" : event.usersLimit,
+				},
+				"defaultEventImage" : event.defaultEventImage,
+				"defaultEventIcon" : event.defaultEventIcon,
+				"participants" : event.participants
+			}
+			, function (err, data) {
+				if (err) {
+					console.error(err);
+					res.status(404).send().end(function () {
+						db.close();
+					});
+					return;
+				} else {
+					res.status(200).send(data).end(function () {
+						db.close();
+					});
+				}
+			  }
+			);
+		});
+    });
+});
+
+router.post('/update', auth, guard.check('user'), function (req, res, next) {
+	tokenHandler.verifyToken(req.payload, function(result) {
+		if (!result) {
+			res.status(401).send("Token is invalid");
+			return;
+		}
+		//dosth
+
+	});
+});
+
+router.post('/joinEvent', auth, guard.check('user'), function (req, res, next) {
+	tokenHandler.verifyToken(req.payload, function(result) {
+		if (!result) {
+			res.status(401).send("Token is invalid");
+			return;
+		}
+		//dosth
+
+	});
+});
+
+
+router.post('/kickUser', auth, guard.check('user'), function (req, res, next) {
+	tokenHandler.verifyToken(req.payload, function(result) {
+		if (!result) {
+			res.status(401).send("Token is invalid");
+			return;
+		}
+		//dosth
+
+	});
+});
+
+
+router.post('/remove', auth, guard.check('user'), function (req, res, next) {
+	tokenHandler.verifyToken(req.payload, function(result) {
+		if (!result) {
+			res.status(401).send("Token is invalid");
+			return;
+		}
+		//dosth
+
+	});
+});
+
+
+router.post('/dezactivate', auth, guard.check('user'), function (req, res, next) {
+	tokenHandler.verifyToken(req.payload, function(result) {
+		if (!result) {
+			res.status(401).send("Token is invalid");
+			return;
+		}
+		//dosth
+
+	});
+});
+
+
+
+router.post('/dezactivateByDate', auth, guard.check('user'), function (req, res, next) {
+	tokenHandler.verifyToken(req.payload, function(result) {
+		if (!result) {
+			res.status(401).send("Token is invalid");
+			return;
+		}
+		//dosth
+
+	});
+});
+
+
+router.post('/dezactivateById', auth, guard.check('user'), function (req, res, next) {
+	tokenHandler.verifyToken(req.payload, function(result) {
+		if (!result) {
+			res.status(401).send("Token is invalid");
+			return;
+		}
+		//dosth
+
+	});
+});
+
+
+router.post('/isActive', auth, guard.check('user'), function (req, res, next) {
+	tokenHandler.verifyToken(req.payload, function(result) {
+		if (!result) {
+			res.status(401).send("Token is invalid");
+			return;
+		}
+		//dosth
+
+	});
+});
+
+
+router.post('/cleanOld', auth, guard.check('user'), function (req, res, next) {
+	tokenHandler.verifyToken(req.payload, function(result) {
+		if (!result) {
+			res.status(401).send("Token is invalid");
+			return;
+		}
+		//dosth
+
+	});
+});
+
+
+router.post('/find', auth, guard.check('user'), function (req, res, next) {
+	tokenHandler.verifyToken(req.payload, function(result) {
+		if (!result) {
+			res.status(401).send("Token is invalid");
+			return;
+		}
+		//dosth
+
+	});
+});
+
+
+router.post('/findFromUser', auth, guard.check('user'), function (req, res, next) {
+	tokenHandler.verifyToken(req.payload, function(result) {
+		if (!result) {
+			res.status(401).send("Token is invalid");
+			return;
+		}
+		//dosth
+
+	});
+});
+
+
+
 module.exports = router;
